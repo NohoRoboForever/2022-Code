0df9af4201f6efa73abc95cd02aa05ede43eaa50
NohoRobo <nohorobo@gmail.com>
2022-02-23 15:18:34 -0800

    Added OI.java

---
+++ src/main/java/frc/robot/OI.java
@@ -0,0 +1,39 @@
+package frc.robot;
+//import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+//import edu.wpi.first.wpilibj.Joystick.ButtonType;
+//import edu.wpi.first.wpilibj.buttons.Button;
+//import edu.wpi.first.wpilibj.buttons.JoystickButton;
+//import edu.wpi.first.wpilibj.command.Command;
+
+//DOCS: https://first.wpi.edu/FRC/roborio/release2017/docs/java/edu/wpi/first/wpilibj/XboxController.html
+
+public class OI {
+    //here, basically set up the all button/joystick bindings for the XboxControllers
+
+    //joystick axes for one controller
+    public static final int RIGHT_AXIS_X = 1;
+    public static final int RIGHT_AXIS_Y = 2;
+    public static final int LEFT_AXIS_X = 3;
+    public static final int LEFT_AXIS_Y = 4;
+
+    //button numbers?
+
+    //controller deadzones
+
+
+    //joystick instantiations
+
+    XboxController sticky = new XboxController(0);
+    // create buttons as buttons of that joystick, w new ports
+
+    public double getJoystickAxis(int analogNumber){
+        return sticky.getRawAxis(analogNumber);
+      }
+    public boolean getButtonValue(int buttonNumber){
+        return sticky.getRawButton(buttonNumber);
+    }
+
+
+    
+}
