66855f081ede824e3767ca54586b5bc26ce56e2c
Noho Robo <nohorobo@gmail.com>
2022-02-28 13:06:52 -0600

    Created all subsystem files

--- src/main/java/frc/robot/subsystems/ClimbArm.java
+++ src/main/java/frc/robot/subsystems/ClimbArm.java
@@ -6,17 +6,12 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
-  /** Creates a new ExampleSubsystem. */
-  public ExampleSubsystem() {}
+public class ClimbArm extends SubsystemBase {
+  /** Creates a new ClimbArm. */
+  public ClimbArm() {}
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-  }
-
-  @Override
-  public void simulationPeriodic() {
-    // This method will be called once per scheduler run during simulation
   }
 }
