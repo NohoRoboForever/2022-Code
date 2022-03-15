0cfd7523e2cafd6334b331b9c3dccf15c41de6df
Noho Robo <nohorobo@gmail.com>
2022-03-15 15:13:48 -0700

    button/controller bindings

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,6 +25,7 @@ public final class Constants {
     //CONSTANT SPEEDS
     public static final double IndexerSpeed = 0.2;
     public final static double climbSpeed = 0.2;
+    public static double defaultTurretSpeed = 0.2;
     public final static double defaultFlywheelSpeed = 0.5;
     public final static double intakeSpeed = 0.5;
 
