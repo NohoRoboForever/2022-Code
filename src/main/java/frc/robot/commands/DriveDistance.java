07b08f71178f653379e1396d566ebd7888aee296
NohoRobo <nohorobo@gmail.com>
2022-03-16 15:49:01 -0700

    DriveDistance actually fucks

--- src/main/java/frc/robot/commands/DriveDistance.java
+++ src/main/java/frc/robot/commands/DriveDistance.java
@@ -47,6 +47,6 @@ public class DriveDistance extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return encoder.getPosition() - distance < 0.1;
   }
 }
