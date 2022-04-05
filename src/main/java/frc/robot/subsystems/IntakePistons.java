7042832c9449c280896d65a124d7724c89c342bd
Noho Robo <nohorobo@gmail.com>
2022-04-05 08:59:29 -0700

    delete print

--- src/main/java/frc/robot/subsystems/IntakePistons.java
+++ src/main/java/frc/robot/subsystems/IntakePistons.java
@@ -23,19 +23,16 @@ public class IntakePistons extends SubsystemBase {
   }
 
   public void toggle() {
-    System.out.println("Toggled");
     leftPiston.toggle();
     rightPiston.toggle();
   }
 
   public void extend() {
-    System.out.println("Extended");
     leftPiston.set(Value.kForward);
     rightPiston.set(Value.kForward);
   }
 
   public void retract() {
-    System.out.println("Retracted");
     leftPiston.set(Value.kReverse);
     rightPiston.set(Value.kReverse);
   }
